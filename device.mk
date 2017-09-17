59c443a6c964c9eda17ea85da071b843df0f7848
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    onclite: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff

--- device.mk
+++ device.mk
@@ -357,7 +357,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.onclite
 
 # Vibrator
 PRODUCT_PACKAGES += \
