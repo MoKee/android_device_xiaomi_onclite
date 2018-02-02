c9f92743ea71b7452b52cf99b2997e93a311e9ad
Wei Wang <wvw@google.com>
2018-02-02 13:01:38 -0800

    onclite: Add SysUI to PRODUCT_DEXPREOPT_SPEED_APPS
    Test: Build
    Change-Id: I088938462b389e1ebe783997f773dc5946345666
    Signed-off-by: fakeyatogod <fakeyatokami@gmail.com>

--- device.mk
+++ device.mk
@@ -168,6 +168,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
