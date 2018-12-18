a4df64f50090c285265780a0289704d4e07c5ac3
Vivek Arugula <varugula@codeaurora.org>
2018-12-18 21:58:24 +0530

    onclite: Enable sensors HAL 2.0
    Change-Id: I96557450db239c8117e7af019629fbdb182d0318

--- device.mk
+++ device.mk
@@ -343,8 +343,9 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0-impl \
+    android.hardware.sensors@2.0-service \
+    android.hardware.sensors@2.0-service.rc \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
