9c0521a8e3966b45b5a5b067b5488184fa1254cb
Bruno Martins <bgcngm@gmail.com>
2019-09-22 14:49:07 +0100

    onclite: Switch to cryptfshw HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33

--- device.mk
+++ device.mk
@@ -192,7 +192,7 @@ PRODUCT_PACKAGES += \
 
 # HW crypto
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+    vendor.qti.hardware.cryptfshw@1.0-service-ioctl-qti
 
 # IMS
 PRODUCT_PACKAGES += \
