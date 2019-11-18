d00f902ef904b4d83248d0f2db634b66b44116e5
Dhina17 <dhinalogu@gmail.com>
2019-11-18 17:30:07 +0000

    onclite: Import keylayouts from stock - onc-user 9 PKQ1.181021.001 V10.3.4.0.PFLMIXM release-keys
    * Cleanup unwanted keylayouts
    * Disable DPAD_CENTER key for fixing the FP tap problem
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -225,10 +225,10 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout xiaomi/onc/onc:10/QKQ1.191008.001/V11.0.2.0.QFLCNXM:user/release-keys
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/keylayout/msm8953-snd-card-mtp_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8953-snd-card-mtp_Button_Jack.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
-	
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
