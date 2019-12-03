e7b5f35189050d04bdf64dbf8c108671a513c532
xNombre <kartapolska@gmail.com>
2019-12-03 20:14:05 +0100

    onclite: Add LMKD props
    * Kang LMKD props from newest google devices (aka Pixels)
    * This makes LMKD spread its wings and become a multitasking beast over kernel side LMK
    
    Change-Id: Ia4e5c84d887eb2071cc969afd4c6455aa7e6b2e6

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -125,6 +125,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.fm.transmitter=false \
     vendor.hw.fm.init=0
 
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/config
