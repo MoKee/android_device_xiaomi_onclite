d2a7426fa7319aebe6035431aee4d5512baab159
mylove90 <engm.saad1990@gmail.com>
2020-01-02 03:18:23 +0100

    onclite: MSM Xtended Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_onclite.mk
+    $(LOCAL_DIR)/xtended_onclite.mk
+
+ COMMON_LUNCH_CHOICES := \
+    xtended_onclite-user \
+    xtended_onclite-userdebug \
+    xtended_onclite-eng
---
+++ xtended_onclite.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from onclite device
+$(call inherit-product, device/xiaomi/onclite/device.mk)
+
+# Inherit some Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := xtended_onclite
+PRODUCT_DEVICE := onclite
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 7
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="onc" \
+    TARGET_DEVICE="onc"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
