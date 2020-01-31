2cf3440b0c5f65cb30294b1cd1397e805b235ea8
Galitsky Dmytro <melles.android@gmail.com>
2020-01-31 18:55:53 +0200

    onclite: include build Keymaster #2

--- device.mk
+++ device.mk
@@ -205,8 +205,10 @@ PRODUCT_PACKAGES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0-impl \
-    android.hardware.keymaster@4.0-service
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
+	android.hardware.keymaster@4.0-service
+
 
 # IMS
 PRODUCT_PACKAGES += \
