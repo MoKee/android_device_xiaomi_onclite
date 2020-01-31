0833a6058c8e55da4c6860580e8c94a676c72ede
krot-melles <melles.android@gmail.com>
2020-01-31 03:48:07 +0000

    Revert "onclite: Enable sensors HAL 2.0"
    This reverts commit a4df64f50090c285265780a0289704d4e07c5ac3.

--- device.mk
+++ device.mk
@@ -343,9 +343,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-impl \
-    android.hardware.sensors@2.0-service \
-    android.hardware.sensors@2.0-service.rc \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
