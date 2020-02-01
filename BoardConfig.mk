f9bcd728dbf40982371e6a321a60f4131c06323e
Galitsky Dmytro <melles.android@gmail.com>
2020-02-01 13:52:54 +0200

    onclite: We use proprietary keymaster HAL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,6 +164,9 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
+# Keymaster
+TARGET_PROVIDES_KEYMASTER := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_onclite
 TARGET_RECOVERY_DEVICE_MODULES := libinit_onclite
