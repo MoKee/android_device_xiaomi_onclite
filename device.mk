47bd873ce049d55dd4a2d5b381ea2959c2de9405
Galitsky Dmytro <melles.android@gmail.com>
2020-02-01 13:54:33 +0200

    onclite: Add gatekeeper HIDL

--- device.mk
+++ device.mk
@@ -219,6 +219,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+#Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
