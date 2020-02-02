f256bc7322531068e3c1789aa31ba7259cd63915
Galitsky Dmytro <melles.android@gmail.com>
2020-02-02 14:08:14 +0200

    onclite: Add keymaster HIDL

--- device.mk
+++ device.mk
@@ -222,6 +222,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+#Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
