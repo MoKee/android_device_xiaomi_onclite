504a494eb2a55693449b14df95b167539cdf5bc6
Galitsky Dmytro <melles.android@gmail.com>
2020-02-02 21:10:41 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -222,16 +222,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
-#Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@3.0-impl
-
-#Gatekeeper
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service \
-    android.hardware.gatekeeper@1.0-impl
-
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
