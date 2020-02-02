6c15aa64b642996b140c91f8885706220d5b2d5b
Galitsky Dmytro <melles.android@gmail.com>
2020-02-02 21:10:05 +0200

    Revert "Update device.mk"
    This reverts commit 3a56a4c1fcd9b7e1fd0f02bd5a68b9cc035dccba.

--- device.mk
+++ device.mk
@@ -222,7 +222,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
-<<<<<<< HEAD
 #Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
@@ -233,8 +232,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0-impl
 
-=======
->>>>>>> parent of f256bc7... onclite: Add keymaster HIDL
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
