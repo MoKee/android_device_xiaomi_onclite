c0a7f3e7730f2f06e07435fb0e980247907f4160
Galitsky Dmytro <melles.android@gmail.com>
2020-02-05 12:59:00 +0200

    onclite: Kang some system props from coral

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -228,3 +228,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.backup.ntpServer=0.pool.ntp.org \
     ro.emmc_size=16GB \
     ro.cutoff_voltage_mv=3400
+	
+	
+# Coral System Props
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_gl_backpressure=1 \
+    debug.sf.early_phase_offset_ns=500000 \
+    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000
