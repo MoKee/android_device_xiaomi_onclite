ff4f0f1cb827a2a98ae9005de74f4f95e8339dd4
SunnyRaj84348 <sunnyraj7461@gmail.com>
2020-02-25 05:23:57 +0530

    onclite: Ram Tweaks

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -267,3 +267,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_app_phase_offset_ns=500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
+
+# Ram Tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+>>>>>>> 1aaa18d... onclite: Ram Tweaks
