d15012946cd2627c7fe6da80701aedf5150dbadd
Galitsky Dmytro <melles.android@gmail.com>
2020-03-21 12:20:06 +0200

    onclite: sepolicy: Update full SEpolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,8 +212,9 @@ TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_PLAT_VENDOR_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/vendor
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Temp
 SELINUX_IGNORE_NEVERALLOWS := true
