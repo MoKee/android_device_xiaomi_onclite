668f4a87055ed12cf09016d1277f3c8084d09749
Galitsky Dmytro <melles.android@gmail.com>
2020-06-02 02:21:09 +0300

    Revert " onclite: Implementing File Based Encryption"
    This reverts commit be2a4f979e6117f5fb5ba2d361a57d6c48ba338b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,9 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 # CNE / DPM
 BOARD_USES_QCNE := true
 
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
