2f71d522f338e55565773db079f60db00baedce2
krot-melles <melles.android@gmail.com>
2020-07-08 23:02:27 +0300

    onclite: Update to Coral's July fingerprint

--- mokee_onclite.mk
+++ mokee_onclite.mk
@@ -27,7 +27,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys" \
