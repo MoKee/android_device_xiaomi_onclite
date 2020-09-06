f25780db8a6dfd3f47016130bef6008beff346f6
krot-melles <melles.android@gmail.com>
2020-09-06 21:26:46 +0300

    onclite: sepolicy: Update denials (#13)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -207,9 +207,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Treble
 BOARD_VNDK_VERSION := current
