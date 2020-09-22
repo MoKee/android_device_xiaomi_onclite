2e34dfb7ceef4f7811e609c38330aa224a500fae
Arian <arian.kulmer@web.de>
2020-09-22 17:32:07 +0200

    onclite: Add a shim for dpmframework
    * libdpmframework depends on strndup16to8 and strdup8to16 methods.
    
    Change-Id: I39d252b5830b752728a0c8607d32d8a279524ce1
    Co-authored-by: Demon000 <demonsingur@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+    product/lib64/libdpmframework.so)
+        patchelf --add-needed "libshim_dpmframework.so" "${2}"
+        ;;
     vendor/lib/hw/camera.msm8953.so)
         patchelf --add-needed camera.msm8953_shim.so "${2}"
         ;;
