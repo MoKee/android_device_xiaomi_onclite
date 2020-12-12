karin722/device_oneplus_guacamoleb

sm8150-common: Add touchscreen gestures HIDL HAL
Change-Id: I02380e387f01fe20f984d2e6daa095f2056c03f9

touch/TouchscreenGesture.cpp
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "TouchscreenGestureService"
+
+#include "TouchscreenGesture.h"
+#include <android-base/logging.h>
+#include <fstream>
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+const std::map<int32_t, TouchscreenGesture::GestureInfo> TouchscreenGesture::kGestureInfoMap = {
+    {0, {251, "Two fingers down swipe", "/proc/touchpanel/double_swipe_enable"}},
+    {1, {254, "Right arrow", "/proc/touchpanel/right_arrow_enable"}},
+    {2, {255, "Down arrow", "/proc/touchpanel/down_arrow_enable"}},
+    {3, {253, "Left arrow", "/proc/touchpanel/left_arrow_enable"}},
+    {4, {247, "Letter M", "/proc/touchpanel/letter_m_enable"}},
+    {5, {250, "Letter O", "/proc/touchpanel/letter_o_enable"}},
+    {6, {248, "Letter S", "/proc/touchpanel/letter_s_enable"}},
+    {7, {246, "Letter W", "/proc/touchpanel/letter_w_enable"}},
+};
+
+Return<void> TouchscreenGesture::getSupportedGestures(getSupportedGestures_cb resultCb) {
+    std::vector<Gesture> gestures;
+
+    for (const auto& entry : kGestureInfoMap) {
+        gestures.push_back({entry.first, entry.second.name, entry.second.keycode});
+    }
+    resultCb(gestures);
+
+    return Void();
+}
+
+Return<bool> TouchscreenGesture::setGestureEnabled(
+    const ::vendor::lineage::touch::V1_0::Gesture& gesture, bool enabled) {
+    const auto entry = kGestureInfoMap.find(gesture.id);
+    if (entry == kGestureInfoMap.end()) {
+        return false;
+    }
+
+    std::ofstream file(entry->second.path);
+    file << (enabled ? "1" : "0");
+    LOG(DEBUG) << "Wrote file " << entry->second.path << " fail " << file.fail();
+    return !file.fail();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor

touch/TouchscreenGesture.h
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_LINEAGE_TOUCH_V1_0_TOUCHSCREENGESTURE_H
+#define VENDOR_LINEAGE_TOUCH_V1_0_TOUCHSCREENGESTURE_H
+
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <vendor/lineage/touch/1.0/ITouchscreenGesture.h>
+#include <map>
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::sp;
+
+class TouchscreenGesture : public ITouchscreenGesture {
+  public:
+    // Methods from ::vendor::lineage::touch::V1_0::ITouchscreenGesture follow.
+    Return<void> getSupportedGestures(getSupportedGestures_cb resultCb) override;
+    Return<bool> setGestureEnabled(const ::vendor::lineage::touch::V1_0::Gesture& gesture,
+                                   bool enabled) override;
+
+  private:
+    typedef struct {
+        int32_t keycode;
+        const char* name;
+        const char* path;
+    } GestureInfo;
+    static const std::map<int32_t, GestureInfo> kGestureInfoMap;  // id -> info
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor
+
+#endif  // VENDOR_LINEAGE_TOUCH_V1_0_TOUCHSCREENGESTURE_H

touch/service.cpp
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.oneplus_msmnile"
+
+#include <android-base/logging.h>
+#include <binder/ProcessState.h>
+#include <hidl/HidlTransportSupport.h>
+#include "TouchscreenGesture.h"
+
+using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
+using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
+
+int main() {
+    android::sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (gestureService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touchscreen HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}

MoKee/android_device_xiaomi_onclite

touch/TouchscreenGesture.cpp
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "TouchscreenGestureService"
+
+#include "TouchscreenGesture.h"
+#include <android-base/logging.h>
+#include <fstream>
+
+namespace vendor {
+namespace mokee {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+const std::map<int32_t, TouchscreenGesture::GestureInfo> TouchscreenGesture::kGestureInfoMap = {
+    {0, {251, "Two fingers down swipe", "/proc/touchpanel/double_swipe_enable"}},
+    {1, {254, "Right arrow", "/proc/touchpanel/right_arrow_enable"}},
+    {2, {255, "Down arrow", "/proc/touchpanel/down_arrow_enable"}},
+    {3, {253, "Left arrow", "/proc/touchpanel/left_arrow_enable"}},
+    {4, {247, "Letter M", "/proc/touchpanel/letter_m_enable"}},
+    {5, {250, "Letter O", "/proc/touchpanel/letter_o_enable"}},
+    {6, {248, "Letter S", "/proc/touchpanel/letter_s_enable"}},
+    {7, {246, "Letter W", "/proc/touchpanel/letter_w_enable"}},
+};
+
+Return<void> TouchscreenGesture::getSupportedGestures(getSupportedGestures_cb resultCb) {
+    std::vector<Gesture> gestures;
+
+    for (const auto& entry : kGestureInfoMap) {
+        gestures.push_back({entry.first, entry.second.name, entry.second.keycode});
+    }
+    resultCb(gestures);
+
+    return Void();
+}
+
+Return<bool> TouchscreenGesture::setGestureEnabled(
+    const ::vendor::mokee::touch::V1_0::Gesture& gesture, bool enabled) {
+    const auto entry = kGestureInfoMap.find(gesture.id);
+    if (entry == kGestureInfoMap.end()) {
+        return false;
+    }
+
+    std::ofstream file(entry->second.path);
+    file << (enabled ? "1" : "0");
+    LOG(DEBUG) << "Wrote file " << entry->second.path << " fail " << file.fail();
+    return !file.fail();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace mokee
+}  // namespace vendor

touch/TouchscreenGesture.h
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_LINEAGE_TOUCH_V1_0_TOUCHSCREENGESTURE_H
+#define VENDOR_LINEAGE_TOUCH_V1_0_TOUCHSCREENGESTURE_H
+
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <vendor/mokee/touch/1.0/ITouchscreenGesture.h>
+#include <map>
+
+namespace vendor {
+namespace mokee {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::sp;
+
+class TouchscreenGesture : public ITouchscreenGesture {
+  public:
+    // Methods from ::vendor::mokee::touch::V1_0::ITouchscreenGesture follow.
+    Return<void> getSupportedGestures(getSupportedGestures_cb resultCb) override;
+    Return<bool> setGestureEnabled(const ::vendor::mokee::touch::V1_0::Gesture& gesture,
+                                   bool enabled) override;
+
+  private:
+    typedef struct {
+        int32_t keycode;
+        const char* name;
+        const char* path;
+    } GestureInfo;
+    static const std::map<int32_t, GestureInfo> kGestureInfoMap;  // id -> info
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace mokee
+}  // namespace vendor
+
+#endif  // VENDOR_LINEAGE_TOUCH_V1_0_TOUCHSCREENGESTURE_H

touch/service.cpp
@@ -17,10 +17,12 @@
 #define LOG_TAG "vendor.mokee.touch@1.0-service.onclite"
 
 #include <android-base/logging.h>
+#include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
 
 #include "KeyDisabler.h"
 #include "GloveMode.h"
+#include "TouchscreenGesture.h"
 
 using android::OK;
 using android::sp;
@@ -32,8 +34,12 @@ using ::vendor::mokee::touch::V1_0::IKeyDisabler;
 using ::vendor::mokee::touch::V1_0::implementation::KeyDisabler;
 using ::vendor::mokee::touch::V1_0::IGloveMode;
 using ::vendor::mokee::touch::V1_0::implementation::GloveMode;
+using ::vendor::mokee::touch::V1_0::ITouchscreenGesture;
+using ::vendor::mokee::touch::V1_0::implementation::TouchscreenGesture;
 
-int main() {
+int main() {	
+	android::sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
     sp<KeyDisabler> keyDisabler;
     sp<GloveMode> gloveMode;
     status_t status;
@@ -68,6 +74,11 @@ int main() {
         goto shutdown;
     }
 
+    if (gestureService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
+        return 1;
+    }
+
     LOG(INFO) << "Touch HAL service is ready.";
     joinRpcThreadpool();
     // Should not pass this line
@@ -75,5 +86,3 @@ int main() {
 shutdown:
     // In normal operation, we don't expect the thread pool to shutdown
     LOG(ERROR) << "Touch HAL service is shutting down.";
-    return 1;
-}