7355ac284b9cdceb6d84acfc3cffc54ec40a3fb2
melles1991 <melles.android@gmail.com>
2020-12-27 12:31:43 +0200

    fix build error

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -215,7 +215,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_VNDK_VERSION := current
