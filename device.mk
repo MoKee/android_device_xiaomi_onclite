d152a50212d9e80f89d26de87bbb4f0c87856559
melles1991 <melles.android@gmail.com>
2020-12-31 06:28:28 +0200

    onclite: Build android.hardware.health@2.1-impl.recovery package For non-A/B devices, install default passthrough implementation to recovery.

--- device.mk
+++ device.mk
@@ -206,7 +206,8 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # HIDL
